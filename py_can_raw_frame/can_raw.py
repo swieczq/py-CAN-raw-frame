7335d44febddf5381433d4fadd157e926c20c8f4
Piotr Sleczka <swieczq.ps@gmail.com>
2022-07-03 13:13:09 +0200

    added basic class, init, basic tests

--- py_can_raw_frame/can_raw.py
+++ py_can_raw_frame/can_raw.py
@@ -1,6 +1,44 @@
 
+from typing import Final
+
+
+CAN_ID_LEN: Final[int]  =11
+BYTE: Final[int]        =8
+DLC_LEN: Final[int]     =4
+
+
+class CanFrame:
+    """Holds and converts CAN frame information to raw can frame"""
+    def __init__(self, *, ID: str, data: str, RTR: str="0", DLC:str=None):
+        #TODO: add safety checks for incorrect input
+        self.ID=ID.zfill(CAN_ID_LEN)
+        self.RTR=RTR
+        if self.RTR=="0":
+            if(len(data)%BYTE==0):
+                proper_datalen=len(data)
+            else:
+                missing_bits=BYTE-len(data)%BYTE
+                proper_datalen=len(data)+missing_bits
+            self.data=data.zfill(proper_datalen)
+        else:
+            self.data=""
+
+        
+        if(self.RTR=="0"):
+            DLC_int=int(len(self.data)/BYTE)
+            self.DLC=bin(DLC_int)[2:] #bin() starts with 0b
+            self.DLC=self.DLC.zfill(DLC_LEN)
+        else:
+            self.DLC=DLC
+
+
+    def __str__(self):
+        return f"ID={self.ID}, data={self.data}, RTR={self.RTR}, DLC={self.DLC}"
+
+
 def main():
-    pass
+    frame=CanFrame(ID="1", data="1")
+    print(str(frame))
 
 if __name__ == "__main__":
     main()
--- tests/test_py_can_raw_frame.py
+++ tests/test_py_can_raw_frame.py
@@ -1,5 +1,27 @@
 from py_can_raw_frame import __version__
+from py_can_raw_frame.can_raw import CanFrame
 
-
+#basic tests
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_CanFrame_init_default_ID():
+    frame=CanFrame(ID="0", data="0")
+    assert frame.ID == "00000000000"
+
+def test_CanFrame_init_default_data():
+    frame=CanFrame(ID="0", data="0")
+    assert frame.data == "00000000"
+
+def test_CanFrame_init_default_DLC():
+    frame=CanFrame(ID="0", data="0")
+    assert frame.DLC == "0001"
+
+#data tests
+def test_CanFrame_init_boundary_condition_data_lower():
+    frame=CanFrame(ID="0", data="11111111")
+    assert frame.data == "11111111"
+
+def test_CanFrame_init_boundary_condition_data_higher():
+    frame=CanFrame(ID="0", data="111111111")
+    assert frame.data == "0000000111111111"
